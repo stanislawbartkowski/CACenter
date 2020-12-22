5b1935405b0628aacf46f03398328ee33eb9a987
Your Name <you@example.com>
2020-12-22 22:27:07 +0100

    alpine jdk

--- restrun.sh
+++ restrun.sh
@@ -4,5 +4,4 @@ source ./env.rc
 
 [[ -d $DIRCA/private ]] || ./ca.sh create force
 
-
-java -cp CARestApi-1.0-SNAPSHOT-jar-with-dependencies.jar com/ca/restapi/CARestApi 9080 ./ca.sh
+java -cp CARestApi-1.0-SNAPSHOT-jar-with-dependencies.jar com/ca/restapi/CARestApi $RESTPORT ./ca.sh
